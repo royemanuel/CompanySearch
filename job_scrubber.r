95bb8f8aedb4cacfee02bbe1e26438b508a16784
Roy N Emanuel II <roy.emanuel@gmail.com>
2020-03-28 10:33:55 -0400

    Large companies with rank

--- job_scrubber.r
+++ job_scrubber.r
@@ -8,6 +8,10 @@ gd_big_url <- "https://www.glassdoor.com/Award/Best-Places-to-Work-LST_KQ0,19.ht
 
 gd_big_html <- read_html(gd_big_url)
 
-gd_big_comp <- html_nodes(gd_big_html, ".strong") %>%
+gd_big_comp <- html_nodes(gd_big_html, ".m-0") %>%
     html_text(.)
+
+top_companies <- as_tibble(as.data.frame(gd_big_comp, col.names = "Company")) %>%
+    mutate(Size = "Large",
+           Rank = as.integer(row.names(top_companies)))
 
